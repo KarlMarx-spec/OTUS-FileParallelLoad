60077d8b4060b104d519383bafdb7ca4ff02190a
Nikita <ariastartup@gmail.com>
2023-11-25 13:36:54 +0300

    Доделал

---
+++ FileLoader.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace OTUS_FileParallelLoad
+{
+    public class FileLoader
+    {
+        public int CheckSpacesIn3Files(string file1, string file2, string file3)
+        {
+            Task<int> t0 = Task.Run(() => CheckSpacesInFile(file1));
+            Task<int> t1 = Task.Run(() => CheckSpacesInFile(file2));
+            Task<int> t2 = Task.Run(() => CheckSpacesInFile(file3));
+            
+            Task.WaitAll();
+
+            return t0.Result + t1.Result + t2.Result;
+        }
+
+        public int CheckSpacesInFilesInPath(string filePath)
+        {
+            int spaceCount = 0;
+
+            try
+            {
+                var files = Directory.GetFiles(filePath);
+
+                foreach (var file in files)
+                {
+                    spaceCount += CheckSpacesInFile(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return spaceCount;
+        }
+
+        public int CheckSpacesInFile(string filePath)
+        {
+            int spaceCount = 0;
+
+            try
+            {
+                using (var reader = new StreamReader(filePath, detectEncodingFromByteOrderMarks: true))
+                {
+                    while (reader.Peek() > -1)
+                    {
+                        if ((char)reader.Read() == ' ')
+                        {
+                            spaceCount++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return spaceCount;
+        }
+
+        public void FileCreate(string filePath, string input)
+        {
+            try
+            {
+                using (FileStream fs = File.Create(filePath))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes(input);
+                    fs.Write(info, 0, info.Length);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OTUS_FileParallelLoad;
+using System.Diagnostics;
+
+Stopwatch timer = new();
+FileLoader fileLoader = new();
+string currentDir = Directory.GetCurrentDirectory();
+
+Console.WriteLine("Создание файлов");
+fileLoader.FileCreate($"{currentDir}/1.txt", "Some interesting text will be here");
+fileLoader.FileCreate($"{currentDir}/2.txt", "Some more interesting text will be here");
+fileLoader.FileCreate($"{currentDir}/3.txt", "Some more and more and more interesting text will be here");
+
+timer.Start();
+var spaces = fileLoader.CheckSpacesIn3Files($"{currentDir}/1.txt", $"{currentDir}/2.txt", $"{currentDir}/3.txt");
+timer.Stop();
+Console.WriteLine($"Результат работы с тремя файлами: {spaces}, Затраченное время: {timer.Elapsed}");
+timer.Reset();
+
+timer.Start();
+spaces = fileLoader.CheckSpacesInFilesInPath(currentDir);
+timer.Stop();
+Console.WriteLine($"Результат работы с файлами в текущей директории: {spaces}, Затраченное время: {timer.Elapsed}");
